bf0f3ee9e926cecab59406d011578a2058301043
Maaz Abdul Basith <maazabdulbasith@gmail.com>
2025-05-16 01:09:41 +0530

    Fixed Jenkinsfile errors

--- server/index.js
+++ server/index.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const connectDB = require('./config/connectDB');
-const router = require('./routes/index');
-const cookiesParser = require('cookie-parser');
-const { app, server } = require('./socket/index');
-const logger = require('./utils/logger'); // Import Winston logger
-const logWithUserId = require('./middleware/loggingMiddleware'); // Import logging middleware
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/connectDB");
+const router = require("./routes/index");
+const cookiesParser = require("cookie-parser");
+const { app, server } = require("./socket/index");
+const logger = require("./utils/logger"); // Import Winston logger
+const logWithUserId = require("./middleware/loggingmiddleware"); // Import logging middleware
 
 // Middleware
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}));
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(cookiesParser());
 app.use(logWithUserId); // Apply the logging middleware
@@ -20,15 +22,15 @@ app.use(logWithUserId); // Apply the logging middleware
 const PORT = process.env.PORT || 8080;
 
 // Root endpoint
-app.get('/', (request, response) => {
-    request.logger('Root endpoint accessed'); // Log the request with userId
+app.get("/", (request, response) => {
+    request.logger("Root endpoint accessed"); // Log the request with userId
     response.json({
-        message: "Server running at " + PORT
+        message: "Server running at " + PORT,
     });
 });
 
 // API endpoints
-app.use('/api', router);
+app.use("/api", router);
 
 // Database connection and server start
 connectDB()
